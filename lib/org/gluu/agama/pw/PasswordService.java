10c747e5906b33d50015489dd61a8ccd1e8e8625
Mostafejur Rahman <mrraju.ice.iu@gmail.com>
2025-04-22 04:34:44 +0600

    refactor: update PasswordService.java

--- lib/org/gluu/agama/pw/PasswordService.java
+++ lib/org/gluu/agama/pw/PasswordService.java
@@ -7,14 +7,6 @@ import java.util.Map;
 
 public abstract class PasswordService {
 
-    public abstract boolean passwordPolicyMatch(String userPassword);
-
-    public abstract Map<String, String> getUserEntityByMail(String email);
-
-    public abstract String sendEmail(String to);
-
-    public abstract String updateUserPassword(String userPassword, String mail);
-
     public abstract boolean validate(String username, String password);
 
     public abstract String lockAccount(String username);
