6e0055627323d34e96d95e333b04e313f539b7a9
Mostafejur Rahman <mrraju.ice.iu@gmail.com>
2025-06-06 23:52:54 +0600

    refactor: update EmailTemplate.groovy

--- lib/org/gluu/agama/pw/jans/EmailTemplate.groovy
+++ lib/org/gluu/agama/pw/jans/EmailTemplate.groovy
@@ -17,7 +17,7 @@ class EmailTemplate {
 <head>
   <meta charset="UTF-8" />
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-  <title>Email Verification</title>
+  <title>Email Verification test</title>
   <link href="https://fonts.googleapis.com/css?family=Nunito+Sans:400,700&display=swap" rel="stylesheet" />
   <style>
     body {
